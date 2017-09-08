c6499b40d0220545808495229071ed785347340f
Nisar <noobards@users.noreply.github.com>
2017-09-08 09:48:41 +0530

    Fixed create account issue

--- application/views/templates/header_logged_out.php
+++ application/views/templates/header_logged_out.php
@@ -1,16 +1,33 @@
-<!DOCTYPE html>
-<html lang="en-us" ng-app="SimKit">
-<head>
-<meta charset="utf-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>SimKit</title><link rel="icon" type="image/x-icon" href="<?php echo base_url(); ?>assets/images/favicon.ico?v=1" />
-<?php echo link_tag("assets/css/bootstrap.min.css"); ?>
-<?php echo link_tag("assets/css/bootstrap.theme.min.css"); ?>
-<?php echo link_tag("assets/css/font_awesome.min.css"); ?>
-<?php echo link_tag("assets/css/style.css"); ?>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/angular.1.6.4.min.js"></script>
-</head>
-<body>
+<!DOCTYPE html>
+
+<html lang="en-us" ng-app="SimKit">
+
+<head>
+
+<meta charset="utf-8" />
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>SimKit</title>
+<link rel="icon" type="image/x-icon" href="<?php echo base_url(); ?>assets/images/favicon.ico?v=1" />
+<?php echo link_tag("assets/css/bootstrap.min.css"); ?>
+
+<?php echo link_tag("assets/css/bootstrap.theme.min.css"); ?>
+
+<?php echo link_tag("assets/css/font_awesome.min.css"); ?>
+
+<?php echo link_tag("assets/css/style.css"); ?>
+
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
+
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/angular.1.6.4.min.js"></script>
+
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/app.js"></script>
+
+</head>
+
+<body>
+
 <div class="container">	
