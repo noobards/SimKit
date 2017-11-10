f750fa26da4ae0cc7b837bd07c4fc54726a1edf3
Nisar <noobards@users.noreply.github.com>
2017-11-10 21:48:14 +0530

    Added credits on login screen

--- application/views/login.php
+++ application/views/login.php
@@ -4,7 +4,7 @@
 
 	<div class="row single">
 
-		<div class="col-md-4 col-md-offset-4">
+		<div class="col-md-6 col-md-offset-3">
 
 			<div class="boxed">
 
@@ -143,14 +143,44 @@
 
 
 	    		<div class="row top10">    		
-
-	    			<div class="col-md-12">
-
+	    			<div class="col-xs-6">						
 	    				<small>&copy; <?= date('Y'); ?> All Rights Reserved</small>
-
 	    			</div>
+					<div class="col-xs-6 text-right">
+						<a ng-click="showCredits($event)" href="#">Big thanks to...</a>
+					</div>
+	    		</div>	
 
-	    		</div>			    				    
+				<div class="row">
+					<div class="col-md-12">
+						<div class="credits" style="display:none;">
+							<h5>A big thank you (<strong>in no particular order</strong>) to the following users:</h5>
+							<ul>								
+								<li><a href="https://www.planetcricket.org/forums/members/thanksrudolph.88834/" target="_blank">ThanksRudolph</a></li>
+								<li><a href="https://www.planetcricket.org/forums/members/pinch-hitter.129787/" target="_blank">Pinch hitter</a></li>
+								<li><a href="https://www.planetcricket.org/forums/members/yash.163359/" target="_blank">Yash.</a></li>
+								<li><a href="https://www.planetcricket.org/forums/members/rudi.141305/" target="_blank">Rudi</a></li>
+								<li><a href="https://www.planetcricket.org/forums/members/presidentevil.140946/" target="_blank">PresidentEvil</a></li>
+								<li><a href="https://www.planetcricket.org/forums/members/rebel2k17.63148/" target="_blank">Rebel2k17</a></li>								
+								<li><a href="https://www.planetcricket.org/forums/members/zwarrior.164901/" target="_blank">zwarrior</a></li>								
+								<li><a href="https://www.planetcricket.org/forums/members/alib.149648/" target="_blank">AliB</a></li>
+								<li><a href="https://www.planetcricket.org/forums/members/cerealkiller.158570/" target="_blank">CerealKiller</a></li>
+								<li><a href="https://www.planetcricket.org/forums/members/freedom.40138/" target="_blank">Freedom</a></li>
+								<li><a href="https://www.planetcricket.org/forums/members/field-marshal.147301/" target="_blank">Field Marshal</a></li>
+								<li><a href="https://www.planetcricket.org/forums/members/xtremegamer.170673/" target="_blank">XtremeGamer.</a></li>
+								<li><a href="https://www.planetcricket.org/forums/members/xeergdef.187281/" target="_blank">Xeergdef</a></li>
+								<li><a href="https://www.planetcricket.org/forums/members/naman-thakur.170104/" target="_blank">Naman Thakur</a></li>
+								<li><a href="https://www.planetcricket.org/forums/members/hawkaussie.75674/" target="_blank">HawkAussie</a></li>
+								<li><a href="https://www.planetcricket.org/forums/members/na-maloom-afraad.110291/" target="_blank">Na Maloom Afraad</a></li>
+								<li><a href="https://www.planetcricket.org/forums/members/sam.151099/" target="_blank">Sam.</a></li>
+								<li><a href="https://www.planetcricket.org/forums/members/swacker.95981/" target="_blank">swacker</a></li>
+								<li><a href="https://www.planetcricket.org/forums/members/akshay.171234/" target="_blank">Akshay.</a></li>
+							</ul>
+							
+							<p>for the continued encouragement and valuable inputs that went into making this simulator. It couldn't have been done without you guys!</p>
+						</div>
+					</div>
+				</div>
 
 	        </div>
 
--- assets/js/pages/login.js
+++ assets/js/pages/login.js
@@ -6,5 +6,5 @@ app.controller('login', function($scope, $http){
 		var button = jQuery(e.target).find('.btn-login');
 		button.attr('disabled', 'disabled').html("Authenticating...");
 		return true;
-	};
+	};		$scope.showCredits = function(e){		e.preventDefault();		var anchor = jQuery(e.currentTarget);		jQuery('.credits').slideToggle();	};
 });
